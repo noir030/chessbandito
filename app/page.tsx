deb45a373bd2b1e3a0e6fb832297d5519cb06a45
Dmitriy Igoshkin <dmitriy.igoshkin@sap.com>
2026-02-12 12:33:23 +0100

    Implement preload

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
+"use client";
+
+import Preloader from "@/components/layout/Preloader";
 import Hero from "@/components/sections/home/Hero";
 import About from "@/components/sections/home/About";
 import Support from "@/components/sections/home/Support";
 
+import { useState, useEffect } from "react";
+import { AnimatePresence } from 'framer-motion';
+
 export default function Home() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (
+      async () => {
+        setTimeout(() => {
+          setIsLoading(false);
+          document.body.style.cursor = 'default'
+          window.scrollTo(0, 0);
+        }, 2100)
+      }
+    )()
+  }, [])
+
   return (
     <main>
+      <AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence>
+
       <Hero />
       <About />
       <Support />
