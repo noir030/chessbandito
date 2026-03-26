eea5052ec5185afa34c4f772bf482c08ca1e4635
Dmitriy Igoshkin <dmitriy.igoshkin@sap.com>
2026-03-26 15:16:28 +0100

    Add Footer

---
+++ components/layout/Footer/Footer.jsx
@@ -0,0 +1,62 @@
+"use client";
+
+import "./Footer.css";
+
+import { useEffect, useState, useRef } from "react";
+import { opacity } from './animation';
+import { useInView, motion } from 'framer-motion';
+
+export default function Footer() {
+  const footerRef = useRef(null);
+  const isInView = useInView(footerRef)
+  const [locationTime, setLocationTime] = useState("");
+
+  useEffect(() => {
+    const updateLocationTime = () => {
+      const options = {
+        timeZone: "Asia/Almaty",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+      };
+
+      const formatter = new Intl.DateTimeFormat("ru-RU", options);
+      const timeString = formatter.format(new Date());
+      setLocationTime(timeString);
+    };
+
+    updateLocationTime();
+    const timeInterval = setInterval(updateLocationTime, 1000);
+
+    return () => clearInterval(timeInterval);
+  }, []);
+
+  return (
+    <footer ref={footerRef}>
+      <div className="container">
+        <div className="footer-header-content">
+          <div className="footer-header">
+            <motion.h1 variants={opacity} animate={isInView ? "open" : "closed"}>Шахматы без границ</motion.h1>
+          </div>
+        </div>
+
+        <div className="footer-byline">
+          <div className="footer-time">
+            <p>
+              Рудный, Казахстан: <span>{locationTime}</span>
+            </p>
+          </div>
+
+          <div className="footer-author">
+            <p>Developed by Mirka</p>
+          </div>
+
+          <div className="footer-copyright">
+            <p>&copy; Отсталая пешка</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
