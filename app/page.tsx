ab2b4b5cd53a00bdb8c52030c948edfb47185cce
Dmitriy Igoshkin <dmitriy.igoshkin@sap.com>
2026-03-28 19:16:40 +0100

    Add description section

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Preloader from "@/components/layout/Preloader/Preloader";
 import Hero from "@/components/sections/home/hero/Hero";
 import Showreel from "@/components/sections/home/showreel/Showreel";
+import Description from '@/components/sections/home/description/Description';
 import Videos from "@/components/sections/home/videos/Videos";
 import TelegramCard from "@/components/sections/home/telegram/TelegramCard";
 import Footer from "@/components/layout/Footer/Footer";
@@ -34,6 +35,7 @@ export default function Home() {
 
       <Hero />
       <Showreel />
+      <Description />
       <Videos />
       <TelegramCard />
       <Footer />
--- components/sections/home/telegram/TelegramCard.tsx
+++ components/sections/home/telegram/TelegramCard.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import "./TelegramCard.css";
-
-import { FaTelegram } from "react-icons/fa";
 import LinkButton from "@/components/ui/Button/LinkButton";
-import { useRef, useEffect } from "react";
+import Button from "@/components/ui/Button/Button";
+
 import gsap from "gsap";
+import { FaTelegram } from "react-icons/fa";
+import { useRef, useEffect } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
@@ -14,54 +15,78 @@ export default function TelegramCard() {
   const sectionRef = useRef<HTMLElement | null>(null);
   const titleRef = useRef<HTMLHeadingElement | null>(null);
   const textRefs = useRef<(HTMLParagraphElement | null)[]>([]);
+  const newsRefs = useRef<(HTMLParagraphElement | null)[]>([]);
 
   useEffect(() => {
-    const items = [titleRef.current, ...textRefs.current].filter(
-      Boolean
-    ) as HTMLElement[];
+    if (!sectionRef.current) return;
 
-    if (!sectionRef.current || !items.length) return;
+    const ctx = gsap.context(() => {
+      const items = [
+        ...newsRefs.current,
+        titleRef.current,
+        ...textRefs.current,
+      ].filter(Boolean) as HTMLElement[];
 
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: sectionRef.current,
-        start: "top 80%",
-        once: true,
-      },
-    });
+      if (!items.length) return;
 
-    tl.fromTo(
-      titleRef.current,
-      {
-        yPercent: 100,
-      },
-      {
-        yPercent: 0,
-        duration: 1.1,
-        ease: "power4.out",
-      }
-    ).fromTo(
-      textRefs.current,
-      {
-        yPercent: 100,
-      },
-      {
-        yPercent: 0,
-        duration: 1,
-        stagger: 0.15,
-        ease: "power4.out",
-      },
-      "-=0.75"
-    );
+      gsap.fromTo(
+        items,
+        {
+          yPercent: 100,
+        },
+        {
+          yPercent: 0,
+          duration: 1,
+          stagger: 0.15,
+          ease: "power4.out",
+          scrollTrigger: {
+            trigger: sectionRef.current,
+            start: "top 80%",
+            once: true,
+          },
+        }
+      );
+    }, sectionRef);
 
     return () => {
-      tl.kill();
+      ctx.revert();
     };
   }, []);
 
   return (
     <section className="telegram" ref={sectionRef}>
       <div className="container">
+        <div className="telegram-copy">
+          <div className="telegram-col">
+            <div className="reveal">
+              <p className="sm">Для справки</p>
+            </div>
+          </div>
+
+          <div className="telegram-col">
+            <div className="reveal">
+              <p
+                className="lg"
+                ref={(el) => {
+                  newsRefs.current[0] = el;
+                }}
+              >
+                У нас на сайте есть последние новости из мира шахмат в формате
+                сторис, просто для информации
+              </p>
+            </div>
+
+            <Button
+              animateOnScroll={true}
+              delay={0.25}
+              variant="dark"
+              href="/news"
+            >
+              Чекнуть новости
+            </Button>
+          </div>
+        </div>
+
         <div className="telegram-card">
           <div className="telegram-card-copy">
             <div className="telegram-card-col">
@@ -78,7 +103,7 @@ export default function TelegramCard() {
                   }}
                 >
                   Это наш закрытый уголок. Здесь просто общаемся про шахматы и
-                  делимся мыслями
+                  делимся мыслями.
                 </p>
               </div>
 
@@ -89,7 +114,7 @@ export default function TelegramCard() {
                   }}
                 >
                   Тут то, что не доходит до YouTube: мысли авторов, новости и
-                  просто общение по душам
+                  просто общение по душам.
                 </p>
               </div>
 
